0f3600ed81226131859a4e6181ef374dc369ecf4
msnurfatih <154402295+msnurfatih@users.noreply.github.com>
2025-08-29 15:13:29 +0700

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,7 @@
 // pages/register.js
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import supabase from '../utils/supabaseClient'
+import { supabase } from '../utils/supabaseClient'
 
 export default function RegisterPage() {
   const router = useRouter()
